a1216de1ee587c748ea32b3f6b53266fa395dba8
HIMANSHU RAI <himanshuuu.2001@gmail.com>
2023-07-23 12:57:14 +0530

    sign up added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,13 +6,15 @@ import Value from "./Components/ValueDiv/Value"
 import Footer from "./Components/FooterDiv/Footer"
 import HomePage from "./Components/HomePage"
 import { Route, Routes } from "react-router-dom"
+import Form from "./Components/Form"
 
 const App = () => {
   return (
     <div>
       <div className="w-[85%] m-auto bg-white">
+        
         <NavBar />
-
+        <Form />
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
